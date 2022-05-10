6327a124680404e3b4f00364b9a93f4ac2175a7c
EverettBazzocchi <contact@everettbazzocchi.ca>
2022-05-10 17:54:46 +0100

    Added Slash Command Support

---
+++ deployCommands.js
@@ -0,0 +1,21 @@
+const fs = require("node:fs");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const config = require("./config.json");
+
+const commands = [];
+
+const commandFiles = fs
+    .readdirSync("./Commands")
+    .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./Commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "9" }).setToken(config.discord.token);
+
+rest.put(Routes.applicationGuildCommands(config.discord.clientID, config.discord.devGuild), { body: commands })
+    .then(() => console.log("Commands created."))
+    .catch(console.error);
